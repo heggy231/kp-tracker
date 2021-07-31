f7b6936d574e45c29dfac6c1eea9b98908c7582a
Heggy Castaneda <heggyy@gmail.com>
2021-07-31 09:18:31 -0400

    config CRUD airtable

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,1 +1,6 @@
 // TODO: create Airtable configuration
+require('dontenv').config();
+const Airtable = require('airtable');
+const base = new Airtable({apikey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+module.exports = { table };
